a3473ab67a329d1053f42f7bd71791462aebdad5
SubhashGupta005 <subhashgupta2520@gmail.com>
2023-08-15 23:22:06 +0530

    test3

--- server.py
+++ server.py
@@ -11,8 +11,8 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     number = request.json
-    result = number['first'] +number['second']
-    return {"result": result }
+    result = number['first'] + number['second']
+    return {"result": result}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
@@ -20,5 +20,5 @@ def subtract():
     result = numbers['first'] - numbers['second']
     return {"result": result}
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
